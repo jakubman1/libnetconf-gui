1173fec463f668413954a2a23ec99b555275fcf7
Jakub Man <jakubman1@gmail.com>
2020-05-27 21:48:26 +0200

    Frontend FIX import error in the YANG Configure tool

--- frontend/projects/tools/src/yang-configure/classes/session.ts
+++ frontend/projects/tools/src/yang-configure/classes/session.ts
@@ -2,11 +2,10 @@
  * Author: Jakub Man <xmanja00@stud.fit.vutbr.cz>
  *
  */
-import {Device} from './device';
 
 export class Session {
   public key: string;
-  public device: Device;
+  public device: object;
   public data?: object[];
   public modifications: object;
 }
